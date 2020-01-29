7b4376a13e3d5e52c22442230f2f8247785ef5af
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-01-28 20:10:55 -0400

    update oauth_url

--- src/pages/Auth/store.js
+++ src/pages/Auth/store.js
@@ -4,7 +4,7 @@ import router from '../../router'
 
 export const store = {
   state: {
-    url_oauth: 'http://localhost:3000/v1/users/github/oauth',
+    url_oauth: `${VUE_APP_API_URL}/users/github/oauth`,
     token: null
   },
   getters: {
